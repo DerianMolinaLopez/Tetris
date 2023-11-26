bf3a7303898ce79d5b50c722894135c174190b9a
Derian Molina Lopez <DerianMolina220@gmail.com>
2023-11-25 23:33:24 -0600

    creando las primeras figuras sin texto

--- Pruebas/PruebaT.asm
+++ Pruebas/PruebaT.asm
@@ -1,105 +1,243 @@
-.model small
-.stack 100h
-PINTAR MACRO COLOR
-   push AX
-   push BX
-   push CX
-   push DX
-   MOV AL,COLOR
-   MOV AH,0CH
-   INT 10H
-   POP DX
-   POP CS
-   POP BX
-   POP DX
-   endm
-   
-CUADRADO MACRO L,X,Y,COLOR
-    PUSH AX
-    PUSH BX
+TITLE TETRIS; DIBUJAR EL MARCO
+.MODEL SMALL
+.STACK 100H
+;POSIBLE PROBLEMA A QUE PUEDE PASAR, ES EL TEMA DE BORRAR LA LINEA, PERO ESO SE DEBE CHECAR CUANDO
+;GENEREE LAS FIGURAS DE FORMA ALEATORIA
+.DATA
+    ATRIB_DISPLAY   DB 10011100B ; Atributos para el display
+    ATRIB_CAMPO     DB 00000000B ; Atributos para el campo
+    ATRIB_BLOQUE    DB 00101110B ; Atributos para el bloque
+    MORAD    DB 00001101B ; Atributos para el bloque (morado)
+    REN             DB 1          ; Posici?n vertical del bloque
+    COL             DB 39         ; Posici?n horizontal del bloque (l?mite izq=20, l?mite der=57)
+    GAME_OVER       DB 0          ; Variable para indicar si el juego ha terminado
+    LETRERO_OVER    DB '     G A M E    O V E R$'
+
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+    ; Configuraci?n del modo de video
+    MOV AH, 0
+    MOV AL, 3
+    INT 10H
+
+    ; Fijar el modo de video 3
+    MOV AH, 5
+    MOV AL, 0
+    INT 10H
+
+    ; Fijar la p?gina activa 0
+    MOV AH, 7
+    MOV AL, 25
+    MOV BH, ATRIB_DISPLAY
+    MOV CX, 0
+    MOV DX, 184FH
+    INT 10H
+
+    ; Dibujar el cuadro alrededor
+    MOV AH, 6
+    MOV AL, 23
+    MOV BH, ATRIB_CAMPO
+    MOV CX, 0114H
+    MOV DX, 173CH
+    INT 10H
+
+    ; Bucle principal del juego
+    CICLO:
+    CALL DIBUJA_CUADRO
+        CALL ESPERA
+        CMP REN, 22
+        JE FIN
+        CALL BORRA_LINEA
+        INC REN
+        JMP CICLO
+
+    FIN:
+    ; Terminar el programa
+    MOV AH, 4CH
+    INT 21H
+
+MAIN ENDP
+DIBUJA_CUADRO PROC
+    ; Mover el cursor a la posici?n REN y COL
+    MOV AH, 2
+    MOV DH, REN
+    MOV DL, COL
+    MOV BH, 0
+    INT 10H
+
+    ; Desplegar el cuadro sin un car?cter
+
+    ; Primer rengl?n del cuadro
+    MOV AH, 9
+    MOV BH, 0
+    MOV AL, ' '   ; Espacio en blanco o cualquier otro car?cter que desees usar
+    MOV CX, 4     ; Longitud del cuadro
+    MOV BL, ATRIB_BLOQUE
+    INT 10H
+
+    ; Segundo rengl?n del cuadro
+    MOV AH, 2
+    MOV DH, REN
+    INC DH
+    MOV DL, COL
+    MOV BH, 0
+    INT 10H
+
+    MOV AH, 9
+    MOV BH, 0
+    MOV AL, ' '   ; Espacio en blanco o cualquier otro car?cter que desees usar
+    MOV CX, 4
+    MOV BL, ATRIB_BLOQUE
+    INT 10H
+
+    RET
+DIBUJA_CUADRO ENDP
+;PARA DIBUAR LA BARRA HORIZONTAL
+DIBUJA_BARRA_HORIZONTAL PROC
+    ; Mover el cursor a la posici?n REN y COL
+    MOV AH, 2
+    MOV DH, REN
+    MOV DL, COL
+    MOV BH, 0
+    INT 10H
+
+    ; Desplegar el cuadro sin un car?cter
+
+    ; Primer rengl?n del cuadro
+    MOV AH, 9
+    MOV BH, 0
+    MOV AL, ' '   ; Espacio en blanco o cualquier otro car?cter que desees usar
+    MOV CX, 18     ; Longitud del cuadro
+    MOV BL, 01101110B ; Establecer el atributo a rojo (bits 0-2 en 4)
+    INT 10H
+
+    ; Segundo rengl?n del cuadro
+    MOV AH, 2
+    MOV DH, REN
+    INC DH
+    MOV DL, COL
+    MOV BH, 0
+    INT 10H
+
+    MOV AH, 9
+    MOV BH, 0
+    MOV AL, ' '   ; Espacio en blanco o cualquier otro car?cter que desees usar
+    MOV CX, 18
+    MOV BL, 01101110B ; Establecer el atributo a rojo (bits 0-2 en 4)
+    INT 10H
+
+    RET
+DIBUJA_BARRA_HORIZONTAL ENDP
+;DUBUJAR LA BARRA VERTICAL
+DIBUJA_BARRA_VERTICAL PROC
+    ; Mover el cursor a la posici?n REN y COL
+    MOV AH, 2
+    MOV DH, REN
+    MOV DL, COL ; A?ade esto para ajustar la columna
+    MOV BH, 0
+    INT 10H
+
+    ; Desplegar el cuadro sin un car?cter
+
+    ; Primer rengl?n del cuadro
+MOV AH, 9
+MOV BH, 0
+MOV AL, ' '   ; Espacio en blanco o cualquier otro car?cter que desees usar
+MOV CX, 4     ; Longitud del cuadro (igual a la anchura actual)
+MOV BL, 01101110B ; Establecer el atributo a rojo (bits 0-2 en 4)
+INT 10H
+
+; Siguiente rengl?n del cuadro
+MOV AH, 2
+INC DH ; Mueve a la siguiente fila
+MOV DL, COL ; Ajusta la columna de nuevo
+MOV BH, 0
+INT 10H
+
+; Repite para las siguientes l?neas
+MOV AH, 9
+MOV BH, 0
+MOV AL, ' '   ; Espacio en blanco o cualquier otro car?cter que desees usar
+MOV CX, 4     ; Longitud del cuadro (igual a la anchura actual)
+MOV BL, 01101110B ; Establecer el atributo a rojo (bits 0-2 en 4)
+INT 10H
+
+MOV AH, 2
+INC DH ; Mueve a la siguiente fila
+MOV DL, COL ; Ajusta la columna de nuevo
+MOV BH, 0
+INT 10H
+    ; Repite esto para cada l?nea adicional...
+
+    RET
+DIBUJA_BARRA_VERTICAL ENDP
+
+
+
+BORRA_LINEA PROC ;ESTO ES PARA QUE BORREMOS LA LINEA QUE IRA DEJANDO A COMO SIGA AVANZANDO LA FIGURA
+    MOV AH, 2
+    MOV DH, REN
+    MOV DL, COL
+    MOV BH, 0
+    INT 10H
+
+    ; Desplegar cuadro
+
+    ; Primer rengl?n del cuadro
+    MOV AH, 9
+    MOV BH, 0
+    MOV AL, ' '
+    MOV CX, 18
+    MOV BL, ATRIB_CAMPO
+    INT 10H
+
+    MOV AH, 2
+    MOV DH, REN
+    INC DH
+    MOV DL, COL
+    MOV BH, 0
+    INT 10H
+
+    ; Segundo rengl?n del cuadro
+    MOV AH, 9
+    MOV BH, 0
+    MOV AL, ' '
+    MOV CX, 18
+    MOV BL, ATRIB_CAMPO
+    INT 10H
+
+    RET
+
+BORRA_LINEA ENDP
+
+ESPERA PROC
     PUSH CX
-    PUSH DX
-    
-    MOV AX,X
-    MOV BX,Y
-    
-    ADD AX,L;LIMITE HORIZONTAL
-    ADD BX,L;LIMITE VERTICAL
-    
-    MOV CX,X
-    MOV DX Y
-    
-S1:    PINTAR COLOR
-    INC DX
-    CMP DX,BX
-    JE S
-    JMP S1
-S: MOV DX,Y 
-   INC CX
-   PINTAR COLOR
-   CMP CX,AX
-   JE FIN
-   JMP S1
-FIN:
-   POP DX
-   POP CX
-   POP
-   BX
-   POz
+    MOV CX, 0FFFFH
 
- 
-    
-    POP DX
+    CICLO_ESPERA:
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        LOOP CICLO_ESPERA
+
     POP CX
-    POP BX
-    POP AX
-ENDM
-    
-    
-.data
-    ; Constantes para el modo gr?fico VGA
-    SCREEN_WIDTH equ 320
-    SCREEN_HEIGHT equ 200
-    SCREEN_SEGMENT equ 0A000h
 
-.code
-main proc
-    mov ax, @data
-    mov ds, ax
+    RET
 
-    ; Configuraci?n del modo gr?fico VGA
-    mov ax, 13h
-    int 10h
+ESPERA ENDP
 
-    ; Calcular las coordenadas del cuadrado
-    mov cx, SCREEN_WIDTH / 2 - 500 ; 200 es la mitad del ancho del cuadrado
-    mov dx, SCREEN_HEIGHT - 1000 ; 400 es la altura del cuadrado
-
-    ; Dibujar el cuadrado rojo
-    mov ax, SCREEN_SEGMENT
-    mov es, ax
-    mov di, cx ; Coordenada x
-    shl di, 1 ; Multiplicar por 2 para obtener el desplazamiento en bytes
-    add di, dx ; Sumar la coordenada y
-    shl di, 8 ; Desplazar 8 bits para obtener el desplazamiento final
-    mov cx, 400 ; Ancho del cuadrado
-    mov dx, 400 ; Altura del cuadrado
-    mov bh, 0 ; P?gina de la pantalla
-    mov ah, 0Ch ; Funci?n para dibujar p?xeles
-    mov al, 4 ; Color rojo
-    int 10h
-
-    ; Esperar una tecla antes de salir
-    mov ah, 0
-    int 16h
-
-    ; Restaurar el modo de texto
-    mov ax, 3
-    int 10h
-
-    ; Salir del programa
-    mov ah, 4Ch
-    int 21h
-main endp
-end main
+END MAIN
 
--- Tetris.asm
+++ Tetris.asm
@@ -1,49 +1,196 @@
-.model small
-.stack 100h
-.data
-    ; Constantes para el modo gr?fico VGA
-    SCREEN_WIDTH equ 320
-    SCREEN_HEIGHT equ 200
-    SCREEN_SEGMENT equ 0A000h
+TITLE TETRIS; DIBUJAR EL MARCO
+.MODEL SMALL
+.STACK 100H
 
-.code
-main proc
-    mov ax, @data
-    mov ds, ax
+.DATA
+    ATRIB_DISPLAY   DB 10011100B ; Atributos para el display
+    ATRIB_CAMPO     DB 00000000B ; Atributos para el campo
+    ATRIB_BLOQUE    DB 00101110B ; Atributos para el bloque
+    REN             DB 1          ; Posici?n vertical del bloque
+    COL             DB 39         ; Posici?n horizontal del bloque (l?mite izq=20, l?mite der=57)
+    GAME_OVER       DB 0          ; Variable para indicar si el juego ha terminado
+    LETRERO_OVER    DB '     G A M E    O V E R$'
 
-    ; Configuraci?n del modo gr?fico VGA
-    mov ax, 13h
-    int 10h
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
 
-    ; Calcular las coordenadas del cuadrado en el centro
-    mov cx, SCREEN_WIDTH / 2 - 20 ; 20 es la mitad del ancho del cuadrado
-    mov dx, SCREEN_HEIGHT / 2 - 20 ; 20 es la mitad de la altura del cuadrado
+    ; Configuraci?n del modo de video
+    MOV AH, 0
+    MOV AL, 3
+    INT 10H
 
-    ; Dibujar el cuadrado rojo
-    mov ax, SCREEN_SEGMENT
-    mov es, ax
-    mov di, cx ; Coordenada x
-    shl di, 1 ; Multiplicar por 2 para obtener el desplazamiento en bytes
-    add di, dx ; Sumar la coordenada y
-    shl di, 8 ; Desplazar 8 bits para obtener el desplazamiento final
-    mov cx, 40 ; Ancho del cuadrado
-    mov dx, 40 ; Altura del cuadrado
-    mov bh, 0 ; P?gina de la pantalla
-    mov ah, 0Ch ; Funci?n para dibujar p?xeles
-    mov al, 4 ; Color rojo
-    int 10h
+    ; Fijar el modo de video 3
+    MOV AH, 5
+    MOV AL, 0
+    INT 10H
 
-    ; Esperar una tecla antes de salir
-    mov ah, 0
-    int 16h
+    ; Fijar la p?gina activa 0
+    MOV AH, 7
+    MOV AL, 25
+    MOV BH, ATRIB_DISPLAY
+    MOV CX, 0
+    MOV DX, 184FH
+    INT 10H
 
-    ; Restaurar el modo de texto
-    mov ax, 3
-    int 10h
+    ; Dibujar el cuadro alrededor
+    MOV AH, 6
+    MOV AL, 23
+    MOV BH, ATRIB_CAMPO
+    MOV CX, 0114H
+    MOV DX, 173CH
+    INT 10H
 
-    ; Salir del programa
-    mov ah, 4Ch
-    int 21h
-main endp
-end main
+    ; Bucle principal del juego
+    CICLO:
+        CALL DIBUJA_BARRA_HORIZONTAL 
+        CALL ESPERA
+        CMP REN, 22
+        JE FIN
+        CALL BORRA_LINEA
+        INC REN
+        JMP CICLO
 
+    FIN:
+    ; Terminar el programa
+    MOV AH, 4CH
+    INT 21H
+
+MAIN ENDP
+DIBUJA_CUADRO PROC
+    ; Mover el cursor a la posici?n REN y COL
+    MOV AH, 2
+    MOV DH, REN
+    MOV DL, COL
+    MOV BH, 0
+    INT 10H
+
+    ; Desplegar el cuadro sin un car?cter
+
+    ; Primer rengl?n del cuadro
+    MOV AH, 9
+    MOV BH, 0
+    MOV AL, ' '   ; Espacio en blanco o cualquier otro car?cter que desees usar
+    MOV CX, 4     ; Longitud del cuadro
+    MOV BL, ATRIB_BLOQUE
+    INT 10H
+
+    ; Segundo rengl?n del cuadro
+    MOV AH, 2
+    MOV DH, REN
+    INC DH
+    MOV DL, COL
+    MOV BH, 0
+    INT 10H
+
+    MOV AH, 9
+    MOV BH, 0
+    MOV AL, ' '   ; Espacio en blanco o cualquier otro car?cter que desees usar
+    MOV CX, 4
+    MOV BL, ATRIB_BLOQUE
+    INT 10H
+
+    RET
+DIBUJA_CUADRO ENDP
+;PARA DIBUAR LA BARRA HORIZONTAL
+DIBUJA_BARRA_HORIZONTAL PROC
+    ; Mover el cursor a la posici?n REN y COL
+    MOV AH, 2
+    MOV DH, REN
+    MOV DL, COL
+    MOV BH, 0
+    INT 10H
+
+    ; Desplegar el cuadro sin un car?cter
+
+    ; Primer rengl?n del cuadro
+    MOV AH, 9
+    MOV BH, 0
+    MOV AL, ' '   ; Espacio en blanco o cualquier otro car?cter que desees usar
+    MOV CX, 4     ; Longitud del cuadro
+    MOV BL, 01101110B ; Establecer el atributo a rojo (bits 0-2 en 4)
+    INT 10H
+
+    ; Segundo rengl?n del cuadro
+    MOV AH, 2
+    MOV DH, REN
+    INC DH
+    MOV DL, COL
+    MOV BH, 0
+    INT 10H
+
+    MOV AH, 9
+    MOV BH, 0
+    MOV AL, ' '   ; Espacio en blanco o cualquier otro car?cter que desees usar
+    MOV CX, 4
+    MOV BL, 01101110B ; Establecer el atributo a rojo (bits 0-2 en 4)
+    INT 10H
+
+    RET
+DIBUJA_BARRA_HORIZONTAL ENDP
+
+
+BORRA_LINEA PROC
+    MOV AH, 2
+    MOV DH, REN
+    MOV DL, COL
+    MOV BH, 0
+    INT 10H
+
+    ; Desplegar cuadro
+
+    ; Primer rengl?n del cuadro
+    MOV AH, 9
+    MOV BH, 0
+    MOV AL, ' '
+    MOV CX, 4
+    MOV BL, ATRIB_CAMPO
+    INT 10H
+
+    MOV AH, 2
+    MOV DH, REN
+    INC DH
+    MOV DL, COL
+    MOV BH, 0
+    INT 10H
+
+    ; Segundo rengl?n del cuadro
+    MOV AH, 9
+    MOV BH, 0
+    MOV AL, ' '
+    MOV CX, 4
+    MOV BL, ATRIB_CAMPO
+    INT 10H
+
+    RET
+
+BORRA_LINEA ENDP
+
+ESPERA PROC
+    PUSH CX
+    MOV CX, 0FFFFH
+
+    CICLO_ESPERA:
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        NOP
+        LOOP CICLO_ESPERA
+
+    POP CX
+
+    RET
+
+ESPERA ENDP
+
+END MAIN
